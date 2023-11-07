8e15eb29483f213e8e7127db272fe655968f32d2
meenatchi <meenatchicse44@gmail.com>
2023-11-07 10:47:56 +0530

    reduce

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,45 @@
-import { createContext, useContext, useState } from "react";
+/* eslint-disable no-undef */
+import { createContext, useContext, useState,useReducer } from "react";
 import "./App.css";
 // step1
+// store
+const store = {
+  prodPrice: 5000,
+  cartCount: 0,
+  totalCost: 0
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "inc_cart_value":
+      return { ...state, cartCount: state.cartCount + 1 };
+    case "dec_cart_value":
+      return { ...state, cartCount: state.cartCount - 1 };
+    case "change_prod_price":
+      return { ...state, prodPrice: action.payload };
+      case "total_cast":
+      return {...state,totalCost:state.prodPrice*state.cartCount}
+    case "buy":
+      return{...state,cartCount:0,totalCost:0}
+      default:
+      return state;
+  }
+};
+
 const shopCtx = createContext(null);
 export default function App() {
-  const [prodPrice, setProductPrice] = useState(5000);
-  const [cartCount, setCartCount] = useState(0);
   const [totalCost, setTotalCost] = useState(0);
 
+  const [state, dispatch] = useReducer(reducer, store);
   return (
     <div className="App">
       {/* step2 creating provider and passing values */}
       <shopCtx.Provider
         value={{
-          cartCount,
-          setCartCount,
-          prodPrice,
-          setProductPrice,
           totalCost,
-          setTotalCost
+          setTotalCost,
+          state,
+          dispatch
         }}
       >
         <ALLComponent />
@@ -38,19 +60,17 @@ function ALLComponent() {
 
 function ProductCard() {
   //step 3 use the context
-  const { prodPrice, cartCount, setCartCount, setTotalCost } = useContext(
-    shopCtx
-  );
+  const {  state, dispatch } = useContext(shopCtx);
   return (
     <div className="prod-card">
       <img src="https://www.skechers.in/on/demandware.static/-/Sites-skechers_india/default/dw17940ed5/images/large/195969748763-1.jpg" />
       <div className="cart-action">
-        <button onClick={() => setCartCount(cartCount + 1)}>+</button>
-        <p>{cartCount}</p>
-        <button onClick={() => setCartCount(cartCount - 1)}>-</button>
+        <button onClick={() => dispatch({ type: "inc_cart_value" })}>+</button>
+        <p>{state.cartCount}</p>
+        <button onClick={() => dispatch({ type: "dec_cart_value" })}>-</button>
       </div>
-      <p>Price {prodPrice}</p>
-      <button onClick={() => setTotalCost(prodPrice * cartCount)}>
+      <p>Price {state.prodPrice}</p>
+      <button onClick={() => dispatch({type:"total_cast"})}>
         checkout
       </button>
     </div>
@@ -58,21 +78,17 @@ function ProductCard() {
 }
 
 function Cart() {
-  const { totalCost, cartCount, setCartCount } = useContext(shopCtx);
+  const {  state, dispatch } = useContext(shopCtx);
   return (
     <div className="prod-card">
       <h3>Items in cart</h3>
       <div className="cart-action">
-        <button onClick={() => setCartCount(cartCount + 1)}>+</button>
-        <p>{cartCount}</p>
-        <button onClick={() => setCartCount(cartCount - 1)}>-</button>
+        <button onClick={() => dispatch({ type: "inc_cart_value" })}>+</button>
+        <p>{state.cartCount}</p>
+        <button onClick={() => dispatch({ type: "dec_cart_value" })}>-</button>
       </div>
-      <h3>Total : {totalCost}</h3>
-      <button
-        onClick={() => {
-          setCartCount(0);
-        }}
-      >
+      <h3>Total : {state.totalCost}</h3>
+      <button onClick={()=>dispatch({type:"buy"})}>
         BUY
       </button>
     </div>
@@ -80,13 +96,15 @@ function Cart() {
 }
 
 function AdminControl() {
-  const { prodPrice, setProductPrice } = useContext(shopCtx);
+  const { state, dispatch } = useContext(shopCtx);
   return (
     <div>
       <input
         type="number"
-        onChange={(e) => setProductPrice(e.target.value)}
-        value={prodPrice}
+        onChange={(e) =>
+          dispatch({ type: "change_prod_price", payload: e.target.value })
+        }
+        value={state.prodPrice}
       />
     </div>
   );
